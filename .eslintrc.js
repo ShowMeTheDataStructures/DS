e0c33f9ffa3557d38c2d3c5b4921f44adcd21e68
haejunejung <jhj9422@unist.ac.kr>
2024-02-05 13:02:03 +0900

    typescript + eslint + prettier + jest

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+// .eslintrc.js
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+};
